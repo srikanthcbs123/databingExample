b9bf64ea9828214f4c4935cbe7021e1507c9ad04
srikanthcbs123 <srikanthcbs123@gmail.com>
2025-02-03 07:59:10 +0530

    Userstory:124547   Databindings code created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'databingExample';
+  //1.Interpoliation Binding
+  // Syntax: {{propertyName}}
+  // • It displays the value of the property in the template.
+  // • When the value of the property is changed, the same value will be automatically updated
+  // in the template.
+
+  //synatx:Propertname:datatype=value;
+  title: String = 'Hello Team'; //this is one property
+  CompanyName: string = 'Microsoft';
+  age: number = 20;
+  isresult: boolean = true;
+  facebookAddress: string = 'https://www.facebook.com/';
+  Firstname: string = 'JNTU COLLEAGE';
+  gender: string = 'Female';
+  Country: string = 'India';
+
+  ChangedDataValues() {
+    debugger;
+    alert("button clicked");
+    this.title = 'AMERICA';
+    this.CompanyName = 'AMAZON';
+    this.age = 100;
+    console.log(this.title);
+    console.log(this.CompanyName);
+    console.log(this.age);
+  }
 }
